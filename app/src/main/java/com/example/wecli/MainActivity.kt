87162b098f8f0535d64444abc599bbdaf7301910
matheusfinamor1 <matheusfinamor1@gmail.com>
2024-07-09 13:55:12 -0300

    withdrawal of requisition requests from Main Activity

--- app/src/main/java/com/example/wecli/MainActivity.kt
+++ app/src/main/java/com/example/wecli/MainActivity.kt
@@ -1,76 +1,24 @@
 package com.example.wecli
 
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
-import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.wecli.ui.theme.WecliTheme
-import com.example.wecli.ui.viewmodel.SplashScreenViewModel
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: SplashScreenViewModel by viewModels()
-    private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
-            if (permissions[android.Manifest.permission.ACCESS_FINE_LOCATION] == true &&
-                permissions[android.Manifest.permission.ACCESS_COARSE_LOCATION] == true
-            ) {
-                viewModel.updateSplashShowValue(false)
-            } else {
-                viewModel.updateSplashShowValue(true)
-            }
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen().apply {
-            setKeepOnScreenCondition {
-                viewModel.splashShowValue.value
-            }
-        }
-        if (!hasLocationPermissions()) {
-            requestLocationPermissions()
-        }
+        installSplashScreen()
         setContent {
             WecliTheme {
                 Column {
                     Text("Ola!")
                 }
             }
-        }
-    }
-
-    private fun hasLocationPermissions(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            android.Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.ACCESS_COARSE_LOCATION
-                ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun requestLocationPermissions() {
-        requestPermissionLauncher.launch(
-            arrayOf(
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            )
-        )
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if (hasLocationPermissions()) {
-            viewModel.updateSplashShowValue(false)
-        } else {
-            viewModel.updateSplashShowValue(true)
         }
     }
 }
