e97a318657973a63923802c6134a721e20eae3cf
matheusfinamor1 <matheusfinamor1@gmail.com>
2024-07-13 16:29:58 -0300

    Change in the way of requesting the user's location, removing the code from the MainActivity and placing it in a composable screen

--- app/src/main/java/com/example/wecli/MainActivity.kt
+++ app/src/main/java/com/example/wecli/MainActivity.kt
@@ -3,27 +3,18 @@ package com.example.wecli
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.wecli.ui.screens.WeatherScreen
 import com.example.wecli.ui.theme.WecliTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
             WecliTheme {
-                Column {
-                    Text("Ola!")
-                }
+                WeatherScreen()
             }
         }
     }
 }
-
-
-
-
-
--- app/src/main/java/com/example/wecli/di/KoinModule.kt
+++ app/src/main/java/com/example/wecli/di/KoinModule.kt
@@ -1,9 +1,7 @@
 package com.example.wecli.di
 
-import com.example.wecli.ui.viewmodel.SplashScreenViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    viewModel { SplashScreenViewModel() }
+
 }
